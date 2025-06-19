eda2109150f66a74c5fcfc58d92a1a5a2c61c6a4
Lev Kadatskyi <levkadatscky@gmail.com>
2025-06-19 11:28:20 +0300

    added code for getting hotel recommendations

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from hotelrecommender!")
+import os
 
+from langchain_community.vectorstores import PGVector
 
-if __name__ == "__main__":
-    main()
+from data.mini_lm_embeddings import MiniLMEmbeddings
+
+embeddings = MiniLMEmbeddings()
+connection_string = os.getenv("DATABASE_URL")
+
+vectorestore = PGVector(
+    connection_string=connection_string,
+    embedding_function=embeddings,
+    collection_name="hotels",
+)
+
+user_query = "I want a nice hotel in New York"
+
+results = vectorestore.similarity_search(user_query, k=3)
+print(results)
+
+print("\nTop hotel recommendations:")
+for doc in results:
+    print(doc)
+    meta = doc.metadata
+    print(f"- {meta['name']} ({meta['location']}): {doc.page_content}")
