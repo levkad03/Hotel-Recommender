eedd1bb6e3a50fa7b7247e92a49fa8832cab3a00
Lev Kadatskyi <levkadatscky@gmail.com>
2025-06-19 13:58:41 +0300

    created basic API for hotel recommendation

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import os
+from typing import List
 
+import uvicorn
+from fastapi import FastAPI
 from langchain_postgres import PGVector
 
 from data.mini_lm_embeddings import MiniLMEmbeddings
+from schemas.hotel_recommendation import HotelRecommendation
+from schemas.recommendation_request import RecommendationRequest
+
+app = FastAPI(
+    title="Hotel Recommender API",
+    description="API for hotel recommendation bot",
+    version="1.0.0",
+)
 
 embeddings = MiniLMEmbeddings()
 connection_string = os.getenv("DATABASE_URL")
-
-vectorestore = PGVector(
+vectorstore = PGVector(
     connection=connection_string,
     embeddings=embeddings,
     collection_name="hotels",
 )
 
-user_query = "I want to stay at hotel with a swimming pool"
 
-results = vectorestore.similarity_search(user_query, k=3)
+@app.post("/recommend", response_model=List[HotelRecommendation])
+def recommend_hotels(request: RecommendationRequest):
+    results = vectorstore.similarity_search(request.query, k=request.k)
+    recommendations = []
 
-print("\nTop hotel recommendations:")
-for doc in results:
-    meta = doc.metadata
-    print(f"- {meta['name']} ({meta['location']}): {doc.page_content}")
+    for doc in results:
+        meta = doc.metadata
+        recommendations.append(
+            HotelRecommendation(
+                name=meta.get("name", "Unknown"),
+                location=meta.get("location", "Unknown"),
+                description=doc.page_content,
+            )
+        )
+
+    return recommendations
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
